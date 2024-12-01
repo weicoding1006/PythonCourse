fb34bb92e5e007317bc8ca5eca83831056749a7d
weicoding1006 <weicoding1006@gmail.com>
2024-12-01 12:21:10 +0800

    Day1

---
+++ Day1/task.py
@@ -0,0 +1,17 @@
+print("Hello World\nHello World")
+
+# input function 可以接收使用者的輸入
+username = input("請問您的姓名？")
+length = len(username)
+print(f"您好！{username}")
+print(f"輸入長度是{length}")
+
+#變數
+string = "字串"
+number = 10
+print(string + str(number))
+
+print("樂團名字創造器")
+firstName = input("城市名稱")
+lastName = input("寵物名字")
+print(f"樂團名稱：{firstName}{lastName}")
